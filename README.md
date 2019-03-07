5c6ff96535c34a840f97479a5159833fddf166cc
chansol, Han <chansol0505@naver.com>
2019-03-07 23:28:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### get motd banner
+http://patorjk.com/software/taag/#p=display&f=Slant%20Relief&t=2012%0AYEOSU%0AEXP0
+### double blackslash banner
+python double-blackslash.py banner.txt
+### setup
+mv motd.sh /etc/update-motd.d/10-help-text
---
+++ double-blackslash.py
@@ -0,0 +1,23 @@
+import sys
+
+filename = sys.argv[1]
+output_sh = filename.rsplit('.', 1)[0]+'.sh'
+print(output_sh)
+
+with open(filename) as f:
+    content = f.read()
+with open(output_sh, 'w') as f:
+    f.write("#!/bin/sh\n")
+    f.write('printf "')
+    for i, c in enumerate(content):
+        if c == '\\':
+            if content[i+1] != '\\':
+                f.write(c)
+            else:
+                f.write('\\\\\\\\')
+        elif c == '\n':
+            f.write('\\n"')
+            f.write(c)
+            f.write('printf "')
+        else:
+            f.write(c)
